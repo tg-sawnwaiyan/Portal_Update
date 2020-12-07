4f2aaf9b3ce09539e15e8fb79783893ef1bf9840
Thet Thiri San <thetthirisan1@gmail.com>
2020-12-07 12:30:46 +0900

    color on News_Category

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -137,7 +137,7 @@ class PostController extends Controller
     {
         $cat_name = Category::where('id',$id)->select('name')->value('name');
 
-        $newslist = Post::where('block_id','!=',0)->where('category_id',$id)->where('recordstatus',1)->orderBy('created_at', 'DESC')->get()->toArray();
+        $newslist = Post::join('categories', 'posts.category_id', '=', 'categories.id' )->select('posts.*','categories.color_code')->where('posts.block_id','!=',0)->where('posts.category_id',$id)->where('posts.recordstatus',1)->orderBy('posts.created_at', 'DESC')->get()->toArray();
 
         $lenght = $tmp = $newarray1 = $newarray2 = $newarray3 = $newarray4 = $aryPush = $aryEmpty = $More = [];
 
