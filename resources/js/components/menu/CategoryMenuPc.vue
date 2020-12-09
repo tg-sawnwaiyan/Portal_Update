e0c5ca84545c9680a41bed1adca5a23bcdfdfb67
sawnwaiyan <sawnwaiyan2011@gmail.com>
2020-12-09 13:16:15 +0900

    remove index var from category menu pc

--- resources/js/components/menu/CategoryMenuPc.vue
+++ resources/js/components/menu/CategoryMenuPc.vue
@@ -5,7 +5,7 @@
                 <span id="left-button" class="left-arr-btn arr-btn d-none-sp" @click="swipeLeft" v-if="is_cat_slided" ><i class="fas fa-angle-left"></i></span>
                 <div class="menu_category" ref="content" v-bind:style="{ width: computed_width }">
                     <ul id="ul_menu_category" class="nav nav-tabs" role="tablist">
-                        <li v-for="cat in cats" :key="cat.id" class="nav-item nav-line tab_color" id="category-id" :style="{'--bkgColor': cat.color_code ? cat.color_code : '#287db4'}" v-bind:value="cat.id" ref="itemWidth" v-on:click="changeBgColor(cat.color_code);">
+                        <li v-for="(cat) in cats" :key="cat.id" class="nav-item nav-line tab_color" id="category-id" :style="{'--bkgColor': cat.color_code ? cat.color_code : '#287db4'}" v-bind:value="cat.id" ref="itemWidth">
                            <router-link v-if="cat.name != 'トップ'"  class="nav-link" :to="{ path:'/newscategory/'+ cat.id}">{{ cat.name }}</router-link>
                            <router-link v-else id="top" class="nav-link" :to="{ path:'/'}">{{ cat.name }}</router-link>
                         </li>
@@ -16,9 +16,7 @@
             </div>      
     </div>
 </template>
-<script>
-import { eventBus } from '../../event-bus.js';
-
+<script scoped>
 export default {
     data(){
         return {
@@ -40,6 +38,7 @@ export default {
     },
     updated: function () {
         this.$nextTick(function () {
+
             // Code that will run only after the
             // entire view has been re-rendered
             var ulWidth = 0;
@@ -54,23 +53,12 @@ export default {
         })
     },
     methods: {
-        changeBgColor(color_code) {
-            eventBus.$emit('gotColor', color_code);
-        },
         getAllCat: function() {           
                 this.axios .get('/api/home') 
                 .then(response => {
-                    this.cats = response.data;
-                    if(this.$route.path === '/'){
-                        eventBus.$emit('gotColor', this.cats[0].color_code);
-                    }else if(this.$route.path.includes('/newscategory')){
-                        this.cats.find(category => {
-                            if (category.id == this.$route.params.id) {
-                                eventBus.$emit('gotColor', category.color_code);
-                            }
-                        })
 
-                    }
+                    this.cats = response.data; 
+
                     });
 
         },
@@ -148,14 +136,14 @@ export default {
     #left-button{
         position: absolute;
         top: 13px;
-        left: 5px;
+        left: 30px;
         padding: 0;
         width: auto;
     }
     #right-button{
         position: absolute;
         top: 13px;
-        right: 5px;
+        right: 30px;
         padding: 0;
         width: auto;
     }
