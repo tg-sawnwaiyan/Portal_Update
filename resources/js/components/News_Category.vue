e448debbff3311370f04ac83fda2fefb76bd5448
gitphyozaw <phyozawoo600@gmail.com>
2020-12-11 09:45:28 +0900

    commit new sp more button add

--- resources/js/components/News_Category.vue
+++ resources/js/components/News_Category.vue
@@ -13,7 +13,7 @@
             </div>                                    
         </div>
     </div>
-    <div class="col-12">
+    <div class="col-12 cat_title">
         <h4 class="profile-tit">{{cat_name}}
         </h4>
     </div>       
@@ -697,26 +697,58 @@
                     </div>
             </div> 
         </div>
-        <div v-if="more_news.length >2" class="form-group text-center head-btn">
+        <div v-if="more_news.length >1" class="form-group text-center head-btn">
             <button type="submit" v-if="!seen" v-on:click="showMoreClick" class="btn_more">もっと見る  <i class='fas fa-chevron-down'></i></button>
             <button type="submit" v-if="seen" v-on:click="showLessClick" class="btn_more"><i class='fas fas fa-chevron-up'></i></button>
         </div> 
     </div>    
     <!-- end layout design -->
    
-    <div v-else-if="block && w_width <= 480" class="row col-12 m-lr-0 p-0">
-            <router-link v-for="(item,index) in news" :key="index" :to="'/newsdetails/'+item.id" class="col-md-6 col-sm-6 col-lg-3 m-b-8 pad-new">
+   <div v-else-if="block && w_width <= 480" class="col-12 m-lr-0 p-0">
+        <div  class="slick-news m-lr-0 bordertop-color">
+            <slick  :options="slickOptions" class="news-slider-width" >
+                <div v-for="(value,index) in big_news" :key="index">
+                    <router-link :to="'/newsdetails/'+value.id" >
+                        <div class="col-6  single-news-box single-news-slide">
+                            <clazy-load class="wrapper-3" @load="log" src="/images/noimage.jpg" :key="index" >
+
+                                <transition name="fade">
+
+                                    <img v-bind:src="'/upload/news/' + value.photo" class="fit-image-0" @error="imgUrlAlt">
+
+                                </transition>                                
+
+                                <transition name="fade" slot="placeholder">
+
+                                    <div class="preloader">
+
+                                        <div class="circle">
+
+                                        <div class="circle-inner"></div>
+
+                                        </div>
+
+                                    </div>
+
+                                </transition>
+                            </clazy-load>
+                            <p>  {{value.main_point}} </p>
+                        </div>
+                    </router-link>
+                </div>
+            </slick>
+        </div>
+        <div v-for="(group,index) in news" :key="index" class="slick-news row m-lr-0 bordertop-color">
+            <router-link v-for="(value,index) in group[0]" :key="index" :to="'/newsdetails/'+value.id" class="col-md-6 col-sm-6 col-lg-3 m-b-8 pad-new">
                 <div class="col-md-12 row adslist-card news-3-card m-0">
 
                     <div class="col-4 img-box">
 
                         <clazy-load class="wrapper-4" @load="log" src="/images/noimage.jpg" :key="index" >
 
-                            <!-- <img v-bind:src="'/upload/news/' + item.photo" class="fit-image" style="height:5rem;width:6rem" @error="imgUrlAlt"> -->
-
                             <transition name="fade">
 
-                                <img :src="'/upload/news/' + item.photo" class="fit-image-0 img-fluid"  @error="imgUrlAlt">
+                                <img :src="'/upload/news/' + value.photo" class="fit-image-0 img-fluid"  @error="imgUrlAlt">
 
                             </transition>
 
@@ -735,10 +767,58 @@
                         </clazy-load>
                     </div>
                     <div class="col-8 pattern-txt-box">
-                        <p> {{item.main_point}} </p>
+                        <p> {{value.main_point}} </p>
                     </div>
                 </div>
             </router-link>
+         
+            <router-link v-for="(value,index) in group[1]" :key="index" :to="'/newsdetails/'+value.id" style="color:#333;" class="col-md-6 col-sm-6 col-lg-3 pad-new">  
+            <p class="text-truncate news-list-display">
+                {{value.main_point}}
+            </p>
+            </router-link>
+        </div>
+        <div v-if="seen" id="more" class="slick-news m-lr-0 bordertop-color" >
+            <router-link v-for="(value,index) in more_news" :key="index" :to="'/newsdetails/'+value.id" class="col-md-6 col-sm-6 col-lg-3 m-b-8 pad-new">
+                <div class="col-md-12 row adslist-card news-3-card m-0">
+
+                    <div class="col-4 img-box">
+
+                        <clazy-load class="wrapper-4" @load="log" src="/images/noimage.jpg" :key="index" >
+
+                            <transition name="fade">
+
+                                <img :src="'/upload/news/' + value.photo" class="fit-image-0 img-fluid"  @error="imgUrlAlt">
+
+                            </transition>
+
+                            <transition name="fade" slot="placeholder">
+
+                            <div class="preloader">
+
+                                <div class="circle">
+
+                                <div class="circle-inner"></div>
+
+                                </div>
+
+                            </div>
+                            </transition>
+                        </clazy-load>
+                    </div>
+                    <div class="col-8 pattern-txt-box">
+                        <p> {{value.main_point}} </p>
+                    </div>
+                </div>
+            </router-link>
+             
+        </div>
+        <div v-if="more_news.length >1" class="form-group text-center head-btn">
+            <button type="submit" v-if="!seen" v-on:click="showMoreClick" class="btn_more">もっと見る  <i class='fas fa-chevron-down'></i></button>
+            <button type="submit" v-if="seen" v-on:click="showLessClick" class="btn_more"><i class='fas fas fa-chevron-up'></i></button>
+        </div> 
+
+       
     </div>
     <div v-else-if="nonblock" class="row col-12 m-lr-0 p-0">
         <router-link v-for="(item,index) in searchnews" :key="index" :to="'/newsdetails/'+item.id" class="col-md-6 col-sm-6 col-lg-3 m-b-8 pad-new">
@@ -781,7 +861,6 @@
     import layout from '../components/home.vue'
     import Slick from 'vue-slick'
     import { eventBus } from '../event-bus.js';
-
 export default {
 
     components: {
@@ -794,6 +873,7 @@ export default {
         return {
             news:[],
             more_news:[],
+            big_news:[],
             cats: [],
             searchnews:[],
             cat_name:'',
@@ -836,6 +916,9 @@ export default {
         }else{
             this.axios.get(`/api/newscategorymobile/${this.$route.params.id}`).then(response => {
                 this.news = response.data.newslist;
+                this.big_news = response.data.bigNews;
+                this.more_news = response.data.moreNews;
+
                 if(response.data.newslist.length == 0)
                 {
                      this.norecord_msg = true;
@@ -891,7 +974,7 @@ export default {
                     },{
                         breakpoint: 420,
                             settings:{
-                                slidesToShow: 1,
+                                slidesToShow: 2,
                                 slidesToScroll:1,
                                 infinite: false
                             }
@@ -1102,6 +1185,15 @@ export default {
     overflow: hidden;
     box-sizing: border-box;
 }
+.single-news-slide{
+    max-width: 100%;
+}
+.single-news-large{
+    float: left;
+}
+ #slick-slide10{
+    width: 170px !important;
+}
 .arr-btn {
     cursor: pointer;
     display: inline-flex;
@@ -1195,11 +1287,44 @@ export default {
 .head-btn{
     margin-top: 8px;
 }
-
+.slick-next, .slick-prev{
+    border: 1px solid #807777;
+    outline: none;
+    background: #f7f7f7;
+    border-radius: 50%;
+    box-shadow: 0 0 0 1px rgba(0,0,0,0.04), 0 4px 8px 0 rgba(0,0,0,0.20);
+}  
+.slick-next::before{
+    border-width: .2rem .2rem 0 0;
+    height: 9px;
+    width: 9px;  
+}
+.slick-prev::before{
+    border-width: .2rem .2rem 0 0;
+    height: 9px;
+    width: 9px;
+}
 @media only screen and (max-width:767px)  {
+    .cat_title{
+        padding: 0 5px;
+    }
     .cat-nav {
         padding-left: 0 !important;
         height: auto !important;
+    }
+    .single-news-box{
+        height: 200px;
+        border: none;
+        background: none;
+        padding: 10px 5px;
+    }
+    .single-news-box .wrapper-3 img{
+        width: 100%;
+        height: 100%;
+    }
+    #more .pad-new{
+        width: 100%;
+        display: inline-block;
     }
 }
 
