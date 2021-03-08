deed5ead58f727360aff6e84dcef36f82d568535
thet <thetthirisan1@gmail.com>
2021-03-08 13:19:13 +0900

    smartnews

---
+++ resources/js/components/smartnews.vue
@@ -0,0 +1,39 @@
+<template>
+
+  
+    <div><pre>{{ xmlContent }}</pre></div>
+ 
+
+</template>
+
+<script>
+export default {
+         data () {
+    return {
+      xmlContent: null,
+    };
+  },
+        created () {
+    this.loadData();
+  },
+  methods: {
+    loadData () {
+      this.xmlContent = null;
+      this.xmlError = false;
+
+      this.axios.get('/xml/smartnews.xml').then(response => {
+          console.log(response);
+        this.xmlContent = response.data;
+      }, () => {
+        this.xmlError = true;
+      });
+    }
+  }
+
+}
+</script>
+<style scoped>
+pre {
+    overflow: hidden;
+}
+</style>
--- resources/js/router.js
+++ resources/js/router.js
@@ -50,6 +50,7 @@ import admincreate from './components/AdminCreate'
 import sitepolicy from './components/sitepolicy'
 import Accountlist from './components/Accountlist'
 import NewsCategory from './components/News_Category'
+import smartnews from './components/smartnews'
 
 
 // import menu from './components/menu/Menu';
@@ -597,6 +598,11 @@ const routes = [
     meta: {
         auth: {roles: 2, redirect: {name: 'login'}, forbiddenRedirect: '/Unauthorized'}
     }
+},
+{
+    name: 'smartnews',
+    path: '/feed/smartnews.xml',
+    component: smartnews,
 },
 ]
 const router = new VueRouter({
