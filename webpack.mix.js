bc394414a204072ecd668ee88998993c3e674034
thet <thetthirisan1@gmail.com>
2021-03-26 11:09:23 +0900

    webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -45,7 +45,7 @@ if (mix.isWatching()) {
 
 
 mix.js('resources/js/app.js', 'public/js')
-   .extract(['vue','bootstrap','quill'])
+   .extract(['vue','bootstrap'])
    .sass('resources/sass/app.scss', 'public/css');
 // mix.css('resources/css/mystyle.css', 'public/css'); //V
 
